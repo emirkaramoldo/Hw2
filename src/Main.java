a479914358037b2c0886f9492646ec3cf394f5d5
emirkaramoldoev <emirkaramoldoev1@gmail.com>
2023-05-05 02:30:47 +0600

    Домашнее задание выполнено.

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(weatherForWalking((int) generateRandomAge(), (int) generateRandomAge()));
+        System.out.println(weatherForWalking((int) generateRandomAge(), (int) generateRandomAge()));
+        System.out.println(weatherForWalking((int) generateRandomAge(), (int) generateRandomAge()));
+        System.out.println(weatherForWalking((int) generateRandomAge(), (int) generateRandomAge()));
+        System.out.println(weatherForWalking((int) generateRandomAge(), (int) generateRandomAge()));
+    }
+
+    public static String weatherForWalking(int age, int temperature){
+
+        if ((age >= 20 && age <= 45) && (temperature >=-20 && temperature <= 30))
+            return "Можно идти гулять";
+        else if(age < 20 && (temperature >=0 && temperature <= 28))
+            return "Можно идти гулять";
+        else if(age > 45 && (temperature >=-10 && temperature <= 25))
+            return "Можно идти гулять";
+        else
+            return "Оставайтесь дома";
+    }
+
+    public static double generateRandomAge(){
+        double randomInt = 1 + Math.random()*99;
+        return randomInt;
+    }
+}
+
